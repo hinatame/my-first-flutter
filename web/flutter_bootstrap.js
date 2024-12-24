b3681475482351c45d2ddfc509812245b0ad63e6
TangKaiCheng <earth791017@gmail.com>
2024-12-24 17:54:06 +0800

    commit two

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3301827190"
+    serviceWorkerVersion: "1162283265"
   }
 });
